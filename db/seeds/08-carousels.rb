5c617b5569049a6f5ba4e05b0ddfc497b6ae6911
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-05 11:29:06 +0800

    Add carousel seed file

---
+++ db/seeds/08-carousels.rb
@@ -0,0 +1,20 @@
+image_list = [
+  Rails.root + "app/assets/images/slide1.jpg",
+  Rails.root + "app/assets/images/slide2.jpg",
+  Rails.root + "app/assets/images/slide3.jpg",
+]
+
+[
+  {
+    image: File.open(image_list[0])
+  },
+  {
+    image: File.open(image_list[1])
+  },
+  {
+    image: File.open(image_list[2])
+  }
+].each do |attributes|
+  carousel = Carousel.new(attributes)
+  carousel.save(validate: false)
+end
