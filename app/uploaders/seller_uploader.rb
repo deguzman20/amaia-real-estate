b2b166f426c214c76e0cc0755e899589069b6ec4
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-05 11:01:37 +0800

    Changed height to 250px

--- app/uploaders/seller_uploader.rb
+++ app/uploaders/seller_uploader.rb
@@ -30,7 +30,7 @@ class SellerUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [318, 151]
+    process resize_to_fit: [318, 250]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
