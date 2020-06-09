d8357de9634d98763587f46c78fac4631492da01
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-06-09 21:03:21 +0800

    Add method

--- app/helpers/sellers_helper.rb
+++ app/helpers/sellers_helper.rb
@@ -2,4 +2,20 @@ module SellersHelper
   def seller_fullname(data)
     "#{data.try(:first_name).capitalize} #{data.try(:middle_name).capitalize} #{data.try(:last_name).capitalize}"
   end
+
+  def seller_position(data)
+    "#{data.try(:position)}"
+  end
+
+  def seller_prc(data)
+    "#{data.try(:prc)}"
+  end
+
+  def seller_hlurb(data)
+    "#{data.try(:hlurb)}"
+  end
+
+  def seller_fb_link(data)
+    "#{data.try(:fb_link)}"
+  end
 end
